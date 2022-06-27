90e47ce748598ba4ce799f1e0f1e97289f033547
maher <gomemahero.com>
2022-06-27 13:25:01 +0300

    firebase in comnnectig and register is work now

---
+++ context/global1.js
@@ -0,0 +1,121 @@
+import { async } from "@firebase/util";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  FacebookAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  sendEmailVerification,
+  sendSignInLinkToEmail,
+  updateProfile,
+} from "firebase/auth";
+
+import {
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  collection,
+  onSnapshot,
+  orderBy,
+  limit,
+  query,
+  where,
+  FieldPath,
+  updateDoc,
+} from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+import { auth, db } from "../firebase2";
+
+
+
+
+const authContext = createContext();
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+const AuthContext = ({ children }) => {
+ 
+  const [currentUser, setUser] = useState({});
+  const [userinfo, setUserinfo] = useState({});
+
+
+
+
+ 
+
+  const signUp = async (email, password, name) => {
+    createUserWithEmailAndPassword(auth, email, password);
+
+    console.log("signUp", email, password, name);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+
+
+
+ 
+
+
+
+
+
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in.
+
+        setUser(user);
+        //  console.log("user status changed: ", user.email, user.uid);
+
+        async function fetchuser() {
+          if (user) {
+            //  console.log(`currentUser: ${user.email}`);
+
+            await getDoc(doc(db, "us111", user.email)).then((userdata) => {
+              //console.log('userdata',userdata)
+          //    setUserinfo(userdata.data());
+              //  console.log("userinf------->>>", userinfo);
+            });
+          }
+        }
+
+        fetchuser().catch(console.error);
+      }
+    });
+    return unsubscribe;
+  }, [currentUser, auth]);
+
+  
+
+  
+
+    
+
+  const value = {
+    signUp,
+    signIn,
+
+    currentUser,
+    userinfo,
+   
+   
+    
+  };
+  return <authContext.Provider {...{ value }}>{children}</authContext.Provider>;
+};
+
+export default AuthContext;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
-
+import Global1 from '../context/global1'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+
+  return (
+    <Global1>
+     <Component {...pageProps} />
+     </Global1>
+
+  )
 }
+
 
 export default MyApp
