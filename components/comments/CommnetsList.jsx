853e5965f495dbdfc1d7ac266b10724c9b841ae2
maher <gomemahero.com>
2022-06-29 16:17:12 +0300

    fix map

--- components/comments/CommnetsList.jsx
+++ components/comments/CommnetsList.jsx
@@ -10,7 +10,7 @@ const CommnetsList = ({comments,userinfo,postid}) => {
 <div>
 
 {comments?.map((comment) => (
-<div className='ml-2 mr-2 min-h-[120px] border-t-2 border-b-2'>
+<div key={comment.id} className='ml-2 mr-2 min-h-[120px] border-t-2 border-b-2'>
 
 <CommentCard postid={postid} comment={comment} userinfo={userinfo} />
 
